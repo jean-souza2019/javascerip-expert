d7c1314babbf3eb789c3538de78c2b7bb4a34f47
Jean Marcos <jeanmarcos@frwk.com.br>
2024-04-04 09:40:52 -0300

    feat: implements rule 3 of readme.md

--- 7-tdd-bdd-project-pt3/src/service/carService.js
+++ 7-tdd-bdd-project-pt3/src/service/carService.js
@@ -1,4 +1,5 @@
 const Tax = require("../entities/tax");
+const Transaction = require("../entities/transaction");
 const BaseRepository = require("../repository/base/baseRepository");
 
 class CarService {
@@ -41,6 +42,30 @@ class CarService {
     const formattedPrice = this.currencyFormat.format(finalPrice)
 
     return formattedPrice
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+
+    const car = await this.getAvailableCar(carCategory)
+    const finalPrice = this.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+    const today = new Date()
+    today.setDate(today.getDate() + numberOfDays)
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+    const dueDate = today.toLocaleDateString('pt-br', options)
+
+    const transaction = new Transaction({
+      customer,
+      dueDate,
+      car,
+      amount: finalPrice
+    })
+
+    return transaction
   }
 }
 
